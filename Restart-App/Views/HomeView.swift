bf3b502822c5cd76e3f11d0147524d1fb88d2529
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-10-11 21:34:19 +0900

    １日目 - UIの作成

---
+++ Restart-App/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Restart-App
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/10/10.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Restart-App/Views/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Restart-App
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/10/10.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
